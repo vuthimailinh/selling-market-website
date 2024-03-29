1d04a0a686f7ae6fce9ae1423a6f39354f02c9a6
AndyKhanhs <linhlaai84@gmail.com>
2024-03-29 11:27:18 +0700

    set up for Signup

--- back-end/index.ts
+++ back-end/index.ts
@@ -7,20 +7,21 @@ import compression from "compression";
 import { json } from "body-parser";
 import testConnection from "./src/database/portgres/connect.portgres";
 import userRouter from "./src/router/User.router";
+import user from "./src/controller/User.controller";
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
-//connect to db
-app.use(userRouter);
-
 // * middleware
 app.use(morganMiddleware);
 app.use(helmet());
 app.use(compression());
 app.use(json());
+
+//connect to db
+app.use(userRouter);
 
 // * router
 const api_version = process.env.API_VERSION || "/api/v1";
--- back-end/src/controller/User.controller.ts
+++ back-end/src/controller/User.controller.ts
@@ -1,25 +1,46 @@
+import { log } from "console";
 import userServices from "../services/User.services";
 
 class User {
     sign_up(req: any, res: any) {
         try {
-            const { name, password } = req.body;
-
-            if (!name || !password) {
-                return res.json({
-                    code: "no input",
-                });
+            const { name, username, password, phone_number, email, role } = req.body;
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const phoneRegex = /^0\d{9}$/;
+            const errors = [];
+            if (!name) {
+                errors.push({message: 'Name is required.' });
             }
-            // luu vao file json
-            // thuc hien logic
-            // tao nguoi dung
-            const mess = userServices.tao_nguoi_dung(name, password);
-
-            return res.json({
-                ok: mess,
-            });
+            if (!username) {
+                errors.push({message: 'Username is required.' });
+            }
+            if (!password) {
+                errors.push({message: 'Password is required.' });
+            }
+            if (!phone_number) {
+                errors.push({message: 'Phone number is required.' });
+            } else if (!phoneRegex.test(phone_number)) {
+                errors.push({message: 'Phone number is not valid.' });
+            }
+            if (!email) {
+                errors.push({message: 'Email is required.' });
+            } else if(!emailRegex.test(email)){
+                errors.push({message: 'Email is not valid.'});
+            }
+            if (!role) {
+                errors.push({message: 'Role is required.' });
+            }
+            
+            // Continue with your logic
+            //const mess = userServices.tao_nguoi_dung(name, password);
+            if(errors.length>0){
+                return res.json({...errors});
+            } else return res.json({
+                message: 'Sign up completed!'
+            })
+            
         } catch (error) {
-            console.log("error");
+            console.log(error);
             return res.json({});
         }
     }
